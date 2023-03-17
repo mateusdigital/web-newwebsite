8981117432d401dff92ee885591b942613895a45
mateus-earth <mateus@cosmicpig.digital>
2023-03-17 12:59:51 -0300

    Fix the projects other other and floss

--- pages/projects.js
+++ pages/projects.js
@@ -21,41 +21,45 @@ export default function Projects() {
             </section>
 
             <section>
-                <h1>FLOSS  <span>Contributions</span></h1>
-                <li><a href="http://www.libreflix.org">libreflix</a>
-                    <a href="https://notabug.org/libreflix/libreflix/pulls/17">(#17)</a>
-                </li>
+                <h1>FLOSS <span>Contributions</span></h1>
+                <ul>
+                    <li>
+                        <span><a href="http://www.libreflix.org">libreflix</a>:</span>
+                        <a href="https://notabug.org/libreflix/libreflix/pulls/17">(#17)</a>
+                    </li>
 
-                <li>Cocos2d-x
-                    <a href="https://github.com/cocos2d/cocos2d-x/pull/16466">(#16466)</a>
-                    <a href="https://github.com/cocos2d/cocos2d-x/pull/16789">(#16789)</a>
-                </li>
+                    <li>
+                        <span>Cocos2d-x:</span>
+                        <a href="https://github.com/cocos2d/cocos2d-x/pull/16466">(#16466)</a>
+                        <a href="https://github.com/cocos2d/cocos2d-x/pull/16789">(#16789)</a>
+                    </li>
 
-                <li>2048py
-                    <a href="https://github.com/davidsousarj/2048py/pull/2">(#2)</a>
-                </li>
+                    <li>
+                        <span>2048py:</span>
+                        <a href="https://github.com/davidsousarj/2048py/pull/2">(#2)</a>
+                    </li>
+                </ul>
             </section>
 
             <section>
                 <h1>Past <span>Work</span></h1>
 
-                <span>Ginga.One</span>
-                <li><a href="https://itunes.apple.com/us/app/pequenos-criativos-school/id1254907412?mt=8">Pequenos Criativos</a> </li>
-                <li><a href="https://itunes.apple.com/br/app/porto-seguro-auto/id940308962?mt=8">Porto Seguro Auto</a> </li>
-                <br/>
+                <ul>
+                    <span><b>Ginga.One</b></span>
+                        <li>Pequenos Criativos <span className="text-detail"> - iOS</span></li>
+                        <li>Porto Seguro Auto <span className="text-detail"> - iOS</span></li>
+                    <br/>
 
-                <span>Imidiar</span>
-                <li>Photo Totem</li>
-                <br/>
+                    <span><b>Imidiar</b></span>
+                        <li>Photo Totem <span className="text-detail"> - Python</span></li>
+                    <br/>
 
-                <span>Eazz</span>
-                <li>Metro Genius</li>
-                <li>donkey.bas</li>
-                <br/>
-
-                <span>Devilware</span>
-                <li>Pirates Treasure</li>
-                <li>Tic Tac Toe</li>
+                    <span><b>Eazz</b></span>
+                        <li>Metro Genius <span className="text-detail"> - Windows Phone </span></li>
+                        <li>donkey.bas <span className="text-detail"> - Windows Phone </span></li>
+                        <li>Pirates Treasure <span className="text-detail"> - Windows Phone </span></li>
+                        <li>Tic Tac Toe <span className="text-detail"> - Windows Phone </span></li>
+                </ul>
             </section>
         </DefaultPage_>
     </>
