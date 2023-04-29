a16572afd60ed106b70ad2bf8ca019fdd19ee029
mateusdigital <mateus@cosmicpig.digital>
2023-04-29 16:45:58 -0300

    Add the contacts page.

---
+++ pages/contacts.js
@@ -0,0 +1,31 @@
+import AboutSection_ from "@/components/about/about-section";
+import DefaultPage_ from "@/components/default-page";
+import Link_ from "@/components/link";
+
+export default function About() {
+  return (
+    <>
+      <DefaultPage_ page_id="contacts">
+
+        <h1>Contact <span>Me</span></h1>
+        <section>
+          <p>
+            Hey there! If you're on the lookout for a skilled professional to add to your team, I'd love to hear from you.
+            <br />
+            I'm currently available and ready to take on new challenges.
+            You can find my contact details below.
+          </p>
+
+          <p>
+            Let's connect!
+          </p>
+          <ul>
+            <li><i class="far fa-envelope"></i> hi@mateus.digital </li>
+            <li><i class="fas fa-phone"></i> +55 35 997 787 567</li>
+            <li><i class="fab fa-linkedin"></i> <a href="https://www.linkedin.com/in/mateus-earth">linkedin.com/in/mateus-earth</a></li>
+          </ul>
+        </section>
+      </DefaultPage_>
+    </>
+  )
+}
