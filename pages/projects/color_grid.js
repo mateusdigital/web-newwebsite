50abf16683c8d1f3840a0a83b1c72d54daa3a95a
mateusdigital <mateus@cosmicpig.digital>
2023-04-29 17:57:59 -0300

    Start to tweak the resume

--- pages/projects/color_grid.js
+++ pages/projects/color_grid.js
@@ -3,16 +3,17 @@ import GetProjectPageInfo from "@/components/project-detail-page/get-project-det
 import DemoPage_ from "@/components/project-detail-page/demo-page";
 
 export default function Page() {
-    const info = GetProjectPageInfo()
-    return (
-        <>
-            <DemoPage_ info={info}>
-                <section>
-                    <p>
-                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur at similique labore sed rerum consequatur quaerat ut deserunt quisquam dicta.
-                    </p>
-                </section>
-            </DemoPage_>
-        </>
-    )
+  const info = GetProjectPageInfo()
+  debugger
+  return (
+    <>
+      {/* <DemoPage_ info={info}>
+        <section>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur at similique labore sed rerum consequatur quaerat ut deserunt quisquam dicta.
+          </p>
+        </section>
+      </DemoPage_> */}
+    </>
+  )
 }
--- pages/resume.js
+++ pages/resume.js
@@ -1,11 +1,21 @@
 import DefaultPage_ from "@/components/default-page";
 
 export default function Resume() {
-    return (
-        <>
-          <DefaultPage_ page="resume">
-                <iframe src="/html/resume.html" frameBorder="0"></iframe>
-          </DefaultPage_>
-        </ >
-    )
+  return (
+    <>
+      <DefaultPage_ page_id="resume">
+        <div className="resumeIFrameContainer">
+
+        <iframe src="html/resume.html" 
+    scrolling="no" 
+    frameborder="0" 
+    frameBorder="0" 
+    marginheight="0px" 
+    marginwidth="0px"
+    allowfullscreen></iframe>
+          {/* <iframe src="/html/resume.html" className="resumeIFrame" scrolling="no"></iframe> */}
+        </div>
+      </DefaultPage_>
+    </>
+  )
 }
