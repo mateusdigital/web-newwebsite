ecf4fc63d941509680dae0fbd17191efe1290136
mateusdigital <mateus@cosmicpig.digital>
2023-05-01 14:20:50 -0300

    Write the about section

--- pages/about.js
+++ pages/about.js
@@ -23,82 +23,127 @@ export default function About() {
         </section>
 
         <AboutSection_ title="latino americano!" photo="srs_1">
-          <p>I'm from a small city called Santa Rita do Sapucai, in the State of Minas Gerais, Brasil!</p>
+          <p>
+            I'm from a small city called Santa Rita do Sapucai, in the state of Minas Gerais, Brazil!
+          </p>
 
-          <p>Nos primeiros 28 anos de minha vida eu vivi e conheci muitos lugares. Minha cidade natal, Contagem, Belo Horizonte, Jundiai, Cajamar, Curitiba, Sao Paulo,</p>
+          <p>
+            In the first 28 years of my life, I lived and visited many places. My hometown,
+            Contagem, Belo Horizonte, Jundiai, Cajamar, Curitiba, and Sao Paulo were some of them.
+          </p>
 
-          <p>Eu tive algumas bandas quando jovem e fiz amigos que carrego hoje.</p>
+          <p>
+            When I was younger, I had a few bands and made friends who I still carry with me today.
+          </p>
 
-          <p>Em 2010 eu entrei para o curso de Ciencia da Computacao em Belo Horizonte, a capital do meu estado. Embora nao tenha concluido o curso, foi nesse periodo que entrei no Microsoft Inovation Center (MicBH).</p>
+          <p>
+            In 2010, I enrolled in the Computer Science course in Belo Horizonte, the capital of my state.
+            Although I didn't complete the course, it was during this time that I joined the Microsoft Innovation Center (MicBH).
+          </p>
 
-          <p>No MicBH eu trabalhei ativamente com, o entao novo, Windows Phone. Alem de pequenos jogos e aplicativos que foram desenvolvidos (eu ganhei 2 lumias novinhos por fazer alguns jogos) eu trabhei bastente com conteudos educacionais sobre a platforma.</p>
+          <p>
+            At MicBH, I worked actively with the then-new Windows Phone. Besides developing small games and applications
+            (I won two brand-new Lumias for making some games), I also worked extensively on educational content about the platform.
+          </p>
 
-          <p>Este esforco me valeu o titulo de Microsoft Student Partner a qual fui pelo periodo de 2 anos (2011-2013). Durante esste tempo eu tive a oportunidade de palestrar em diversos centros academicos e universidades, em especial na Universdade Federal de Itajuba, No Instutudo de Telecomuicacoes e Pontificia Universade Catolica de Minas Gerais - e outros.</p>
+          <p>
+            This effort earned me the title of Microsoft Student Partner, which I held for a period of two years (2011-2013).
+            During this time, I had the opportunity to speak at various academic centers and universities,
+            especially at the Federal University of Itajuba, the Telecommunications Institute,
+            and the Pontifical Catholic University of Minas Gerais, among others.
+            <br /><br />
+            I even got on the newspaper ":)"
+          </p>
 
-          <p>Esse foi um periodo de muito estudo no qual me resulto em duas certificoes da Microsoft:</p>
-          {/* <li>Microsoft - MTA-98-373</li>
-                    <li>Microsoft - MTA-98-361</li> */}
-          E mais de 123 outras certificoes menores.
+          <p>
+            This was a period of intense study that resulted in two Microsoft certifications for me:
+          </p>
+
+          <li>Microsoft - MTA-98-373</li>
+          <li>Microsoft - MTA-98-361</li>
+
+          <p>
+            And more than 123 other minor certifications.
+          </p>
+
+          <p>
+            In 2016, I became a programmer at Firehorse Studios in Sao Paulo, an opportunity that allowed
+            me to work on games like The Banner Saga 1 and 2, Killers and Thieves, and others.
+          </p>
         </AboutSection_>
 
 
         <AboutSection_ title="LISBON" photo="lisbon">
           <p>
-            No final de 2017 eu recebi uma ligacao da Miniclip me convidado para ir para me juntar ao time da
-            Miniclip Porgutal, em Maio de 2018 eu estava pela primeira vez saindo do pais!
+            At the end of 2017, I was invited to join the Miniclip Portugal team, and in May 2018,
+            I was leaving the country for the first time!
           </p>
           <p>
-            Em Portugal eu fiz os melhores amigos da minha vida e visitei lugares incrivelmente interessantes.
-            Nesse tempo eu vistei a Croacia, a Franca, a Espanha e quase todo Portugal.
+            While in Portugal, I made the best friends of my life and visited incredibly interesting places.
+            During that time, I visited Croatia, France, Spain, and almost all of Portugal.
+          </p>
+          <p>
+            In addition to working on Pacman.io and 8 Ball Pool, I co-authored The Mistral Report
+            with <a href="https://montyontherun.itch.io">Monty</a>, a 1st person stealth dungeon crawler
+            for MS-DOS that won the <a href="http://www.retrozaragoza.com/">Retro Zaragozza 2018</a>🏆.
           </p>
 
           <p>
-            Alem de trabalhar no Pacman.io e no 8 Ball Pool, eu fui co-author junto com  <a href="https://montyontherun.itch.io">Monty</a>
-            de The Mistral Report,  1st person stealth dungeon crawler for MS-DOS that won the <a href="http://www.retrozaragoza.com/">Retro Zaragozza 2018</a>🏆.
-
-            Com o intresse em retro computer em alta eu lancei o El Jamon Volador para o Game Boy.
+            With the interest in retro computers high, I also launched El Jamon Volador for the Game Boy.
           </p>
+
         </AboutSection_>
 
         <AboutSection_ title="Minsk" photo="belarus_1">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam veniam repellendus reiciendis nemo culpa voluptatem labore assumenda similique deserunt.
+            In the end of 2019, my career progressed with an invitation to join the Wargaming team, and in October,
+            I moved to the capital of Belarus. Initially, the cultural shift and language barrier proved to be
+            challenging, but the sheer excitement helped me overcome it!
           </p>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa.
+            At Wargaming, I worked on two games. First, as a Server Programmer, I was responsible for implementing
+            the game metaservers. Unfortunately, I'm under NDA, so I can't discuss it further.
+            Secondly, I worked with Unreal Engine 4 to create UI and some gameplay elements for the successful World of Tanks - Blitz.
           </p>
 
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet itaque libero earum dolore magnam magni, et, nisi perferendis iusto dicta numquam voluptas, eveniet tempora officia nulla nihil ratione maiores dolorem.
+            I also started to learn Russian as my third language, and things were going well,
+            but this changed with the 2020 elections.
           </p>
         </AboutSection_>
 
         <AboutSection_ title="Kyiv" photo="kyiv_4">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam veniam repellendus reiciendis nemo culpa voluptatem labore assumenda similique deserunt.
+            Due to political instability following the 2020 elections in Belarus, I decided to accept an offer
+            from Ubisoft Kyiv to join as Senior Programmer for the upcoming Skull and Bones.
           </p>
-
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa.
+            My job was to implement state-of-the-art accessibility solutions, which focused on text-to-speech
+            and speech-to-text. I worked with a multi-studio effort involving Kyiv, Odessa, Berlin, and Belgrade.
           </p>
-
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet itaque libero earum dolore magnam magni, et, nisi perferendis iusto dicta numquam voluptas, eveniet tempora officia nulla nihil ratione maiores dolorem.
+            Kyiv was the most awesome place I had lived in to date, with a vibrant culture, wonderful places
+            to chill or hang out with friends, and the hospitality of the people I met made me love the city.
+          </p>
+          <p>
+            After living in Kyiv for two years and starting a family, the situation became unbearable
+            when the Russian Federation started a special military operation on the territory of
+            Ukraine on February 24th, 2023, forcing me to flee...
           </p>
         </AboutSection_>
 
         <AboutSection_ title="Brasil again..." photo="guaruja_1">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius quam veniam repellendus reiciendis nemo culpa voluptatem labore assumenda similique deserunt.
+            After deciding to leave the territory of Ukraine, I felt it was time for a break. My daughter was about
+            to be born, and the outcome of the invasion was unclear, so we decided to move back to Brazil.
           </p>
-
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, ipsa.
+            Initially, my family and I lived in the coastal city of Guaruja, where Marla was born, and we enjoyed
+            long walks at sunset with our dog, Pingo.
           </p>
-
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet itaque libero earum dolore magnam magni, et, nisi perferendis iusto dicta numquam voluptas, eveniet tempora officia nulla nihil ratione maiores dolorem.
+            Later on, we decided it would be wise to move closer to family and returned to my hometown.
           </p>
         </AboutSection_>
 
