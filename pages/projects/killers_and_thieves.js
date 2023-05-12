8bb67209793bba3e8f251283783a906baabb0a47
mateusdigital <mateus@cosmicpig.digital>
2023-05-12 11:42:48 -0300

    Killers and thieves wrote

--- pages/projects/killers_and_thieves.js
+++ pages/projects/killers_and_thieves.js
@@ -1,24 +1,27 @@
 
 import GetProjectPageInfo from "@/components/project-detail-page/get-project-detail-page-info";
-import ProfessionalPage_  from "@/components/project-detail-page/professional-page";
+import ProfessionalPage_ from "@/components/project-detail-page/professional-page";
 
 
 export default function Page() {
-    const info = GetProjectPageInfo()
-    return (
-        <>
-            <ProfessionalPage_ info={info}>
-                <section>
-                    <p>
-                        Improve game for publishing by adding features and fixing bugs.
-                        Work extensively to optimize Lua and Unity interop code.
-                        Implemented UI features in Unity and Gameplay features in Lua.
-                        Implement and refine AI algorithms for heists.
-                        Implement Steamworks API.
-                        Worked with: Unity, Lua, C#, and Git
-                    </p>
-                </section>
-            </ProfessionalPage_>
-        </>
-    )
+  const info = GetProjectPageInfo()
+  return (
+    <>
+      <ProfessionalPage_ info={info}>
+        <section>
+          <p>
+            After my work at Stoic's Studio The Banner Saga, I've invited to work in Killers and Thieves, a
+            game that they would be publishing.
+          </p>
+          <p>
+            I had improved game for publishing by adding features and fixing bugs.
+            Also extensively work was done to optimize Lua and Unity interop code, implement UI features in Unity and game play features in Lua.
+          </p>
+          <p>
+            Besides Implement Steamworks API integration I've implemented and refine AI algorithms for heists.
+          </p>
+        </section>
+      </ProfessionalPage_>
+    </>
+  )
 }
