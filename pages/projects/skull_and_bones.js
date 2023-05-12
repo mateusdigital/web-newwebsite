ca60bc42b54ad0debd0886925bcffc3ee5eb2cbd
mateusdigital <mateus@cosmicpig.digital>
2023-05-12 11:21:17 -0300

    Write s&b

--- pages/projects/skull_and_bones.js
+++ pages/projects/skull_and_bones.js
@@ -1,22 +1,26 @@
 
 import GetProjectPageInfo from "@/components/project-detail-page/get-project-detail-page-info";
-import ProfessionalPage_  from "@/components/project-detail-page/professional-page";
+import ProfessionalPage_ from "@/components/project-detail-page/professional-page";
 
 
 export default function Page() {
-    const info = GetProjectPageInfo()
-    return (
-        <>
-            <ProfessionalPage_ info={info}>
-                <section>
-                    <p>
-                        After working in Skull and Bones I went to help the release of LOST BETWEEN WORLDS DLC.
+  const info = GetProjectPageInfo()
+  return (
+    <>
+      <ProfessionalPage_ info={info}>
+        <section>
+          <p>
+            In 2020 I joined Ubisoft Kyiv as Senior Programmer. My job was to implement state-of-the-art <b>ACCESSIBILITY</b> solutions, 
+            which focused on <b>text-to-speech</b> and <b>speech-to-text</b>. <br/>
+            I worked with a multi-studio effort involving Kyiv, Odessa, Berlin, and Belgrade. 
+          </p>
 
-                        Bug fixing for <i>A11Y</i> components.
-                        Worked with Ubisoft's Dunia engine and Phoenix UI.
-                    </p>
-                </section>
-            </ProfessionalPage_>
-        </>
-    )
+          <p>
+            In this project I've the opportunity to work with <b>Anvil</b> (the same engine that powers Assasins Creed) 
+            and <b>Phoenix UI</b> (the UI engine that Rainbow Six uses).
+          </p>
+        </section>
+      </ProfessionalPage_>
+    </>
+  )
 }
