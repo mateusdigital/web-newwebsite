f172968b58331513e45bcb1ede7e2160fe67971f
mateusdigital <mateus@cosmicpig.digital>
2023-05-12 11:49:54 -0300

    Write let them come and at sundown

--- pages/projects/at_sundown.js
+++ pages/projects/at_sundown.js
@@ -1,21 +1,20 @@
+import Link_ from "@/components/link";
 import GetProjectPageInfo from "@/components/project-detail-page/get-project-detail-page-info";
-import ProfessionalPage_  from "@/components/project-detail-page/professional-page";
+import ProfessionalPage_ from "@/components/project-detail-page/professional-page";
 
 export default function Page() {
-    const info = GetProjectPageInfo()
-    return (
-        <>
-            <ProfessionalPage_ info={info}>
-                <section>
-                    <p>
-                        In this project I was responsible to integrate the AlienFx SDK to YoYo Games' Game Maker.
-                        The AlienFx SDK is written in C++ and provided as an DLL file, but it isn't readily usable in Game Maker.
-                        My job was write a binding that maps more 1:1 possible the C++ in GML (the Game Maker scripting language).
-                        It was very challenging since I had no experience in Game Maker at all, but at end we managed to get it right.
-                        I write some shell scripting to ease the studio day to day stuff and I wrote an texture packer algorithm to improve the performance of the particle system.
-                    </p>
-                </section>
-            </ProfessionalPage_>
-        </>
-    )
+  const info = GetProjectPageInfo()
+  return (
+    <>
+      <ProfessionalPage_ info={info}>
+        <section>
+          <p>
+            Similar to the <Link_ href="/projects/let_them_come">Let them Come</Link_> my work here 
+            was to create bindings to the AlienFx SDK in Unity, so the game developers would have 
+            access to it in the environment that they're used to.
+          </p>
+        </section>
+      </ProfessionalPage_>
+    </>
+  )
 }
--- pages/projects/let_them_come.js
+++ pages/projects/let_them_come.js
@@ -1,21 +1,25 @@
 import GetProjectPageInfo from "@/components/project-detail-page/get-project-detail-page-info";
-import ProfessionalPage_  from "@/components/project-detail-page/professional-page";
+import ProfessionalPage_ from "@/components/project-detail-page/professional-page";
 
 export default function Page() {
-    const info = GetProjectPageInfo()
-    return (
-        <>
-            <ProfessionalPage_ info={info}>
-                <section>
-                    <p>
-                        In this project I was responsible to integrate the AlienFx SDK to YoYo Games' Game Maker.
-                        The AlienFx SDK is written in C++ and provided as an DLL file, but it isn't readily usable in Game Maker.
-                        My job was write a binding that maps more 1:1 possible the C++ in GML (the Game Maker scripting language).
-                        It was very challenging since I had no experience in Game Maker at all, but at end we managed to get it right.
-                        I write some shell scripting to ease the studio day to day stuff and I wrote an texture packer algorithm to improve the performance of the particle system.
-                    </p>
-                </section>
-            </ProfessionalPage_>
-        </>
-    )
+  const info = GetProjectPageInfo()
+  return (
+    <>
+      <ProfessionalPage_ info={info}>
+        <section>
+          <p>
+            In this project I was responsible to integrate the AlienFx SDK to YoYo Games' Game Maker.
+            The AlienFx SDK is written in C++ and provided as an DLL file, but it isn't readily usable in Game Maker.
+            <br/>
+            My job was write a binding that maps more 1:1 possible the C++ in GML (the Game Maker scripting language).
+            It was very challenging since I had no experience in Game Maker at all, but at end we managed to get it right.
+          </p>
+
+          <p>
+            I also wrote an texture packer algorithm to improve the performance of the particle system.
+          </p>
+        </section>
+      </ProfessionalPage_>
+    </>
+  )
 }
