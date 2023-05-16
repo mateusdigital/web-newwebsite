b6329a90cbdb7bd7b1a43a002eb83e5a72d470f0
mateusdigital <mateus@cosmicpig.digital>
2023-05-16 15:00:04 -0300

    ./scripts/deploy.sh - Build: 17

---
+++ mateusdigital.github.io/src/main.js
@@ -0,0 +1,21 @@
+(function() {
+    const canvas       = document.getElementById("projCanvas");
+    const project_name = canvas.dataset.projectName.toLowerCase();
+    const base_path    =`/modules/demos/${project_name}`;
+
+    demolib_load_script(base_path, `/source/main.js`).then(()=>{
+        demolib_load_all_scripts(base_path, __SOURCES).then(()=>{
+            if(!canvas) {
+                debugger;
+            }
+
+            demo_main(canvas);
+        }).catch((e) => {
+            console.error(e);
+            debugger;
+        });
+    }).catch((e)=> {
+        console.error(e);
+        debugger;
+    })
+})();
