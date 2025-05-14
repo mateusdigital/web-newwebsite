2505e351cf7916de1b87e0c64c01119cb60c882d
mateusdigital <hello@mateus.digital>
2025-05-14 19:50:47 -0300

    body padding...

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,30 +1,37 @@
+import { useState } from "react";
 import Link_ from "@/components/link";
 
-import GamesIcon_    from "@/components/icons/gamepad-solid";
+import GamesIcon_ from "@/components/icons/gamepad-solid";
 import ProjectsIcon_ from "@/components/icons/layer-group-solid";
 import ContactsIcon_ from "@/components/icons/comments-solid";
-import ResumeIcon_   from "@/components/icons/scroll-solid";
-import AboutIcon_    from "@/components/icons/address-card-solid";
+import ResumeIcon_ from "@/components/icons/scroll-solid";
+import AboutIcon_ from "@/components/icons/address-card-solid";
 
 export default function NavBar_({ page_id }) {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => setMenuOpen(o => !o);
+
   return (
     <>
       <nav className="topNav">
-        <div className="topNavLogoContainer">
-          <Link_ className="topNavLogo" href="/">mateus.digital</Link_>
-        </div>
+        <Link_ className="logo" href="/">mateus.digital</Link_>
+        <button className="hamburger" onClick={toggleMenu}>
+          ☰
+        </button>
 
-        <div className="topNavButtonsContainer">
-          <Link_ className={`topNavButton ${page_id == "games"    ? "topNavButtonActive" : ""}`}  href="/games"><GamesIcon_/><span>Games</span></Link_>
-          <Link_ className={`topNavButton ${page_id == "projects" ? "topNavButtonActive" : ""}`}  href="/projects"><ProjectsIcon_/><span>Projects</span></Link_>
-          <Link_ className={`topNavButton ${page_id == "contacts" ? "topNavButtonActive" : ""}`}  href="/contacts"><ContactsIcon_/><span>Contacts</span></Link_>
-          <Link_ className={`topNavButton ${page_id == "resume"   ? "topNavButtonActive" : ""}`}  href="/resume"><ResumeIcon_/><span>Resume</span></Link_>
-          <Link_ className={`topNavButton ${page_id == "about"    ? "topNavButtonActive" : ""}`}  href="/about"><AboutIcon_/><span>About</span></Link_>
+        {menuOpen && (
+          <div className="dropdownMenu">
+            <div className="dropdownMenuItemsContainer" onClick={toggleMenu} >
+            <Link_ className={`menuItem ${page_id === "games" ? "active" : ""}`} href="/games"><GamesIcon_ /><span>Games</span></Link_>
+            <Link_ className={`menuItem ${page_id === "projects" ? "active" : ""}`} href="/projects"><ProjectsIcon_ /><span>Projects</span></Link_>
+            <Link_ className={`menuItem ${page_id === "contacts" ? "active" : ""}`} href="/contacts"><ContactsIcon_ /><span>Contacts</span></Link_>
+            <Link_ className={`menuItem ${page_id === "resume" ? "active" : ""}`} href="/resume"><ResumeIcon_ /><span>Resume</span></Link_>
+            <Link_ className={`menuItem ${page_id === "about" ? "active" : ""}`} href="/about"><AboutIcon_ /><span>About</span></Link_>
 
-          {/* @todo as fa-caret-down, fa-chevron-down, and fa-angle-down.  */}
-          {/* <Link_ className="hamburger" href=""><i className="fas fa-bars"></Link_> */}
-        </div>
+            </div>
+          </div>
+        )}
       </nav>
     </>
-  )
+  );
 }
